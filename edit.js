c3657b9b1d4bce1299bc91e7323dde1f9971c728
amin7942 <76994595+amin7942@users.noreply.github.com>
2023-10-11 18:43:54 +0900

    업뎃1

--- edit.js
+++ edit.js
@@ -2,3 +2,12 @@
 function edit(){
    confirm("게시물을 수정 할까요?")
 }
+
+function valid(){
+    const input = document.getElementById("edit_input");
+    if(!input.value){
+        alert("수정할 내용을 입력하세요.")
+    }else{
+        window.location.href="index.html";
+    }
+}
--- save.js
+++ save.js
@@ -5,9 +5,20 @@ function save_data(){
     
     for(var i = 0; i < x.length; i++){
         text[i] = x[i].value;
-        console.log(text);
     }
-    alert("게시물이 추가됩니다.")
+    if(!text[0]) {
+        alert("닉네임을 입력 해 주세요.")
+    }
+    else if(!text[1]){
+        alert("서버명을 입력 해 주세요.")
+    }
+    else if(!text[2]){
+        alert("블랙리스트 추가 이유를 입력 해 주세요.")
+    }
+    else{
+        alert("게시물이 추가됩니다.");
+        window.location.href="idex.html";
+    }
 }
 
 
